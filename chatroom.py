13c3581b3dcce0027b04684dbf0e5595ed8b93fa
Abdul-01-lahi <abdullahioye10@gmail.com>
2023-01-19 11:25:34 +0100

    added an image

---
+++ chatroom.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect
+from datetime import datetime
+from models.message import Message
+
+
+app = Flask(__name__)
+
+messages = []
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    if request.method == "POST":
+        user_name = request.form["user-name"]
+        user_message = request.form["user-message"]
+        messages.insert(
+            0,
+            Message(
+                sender=user_name,
+                message=user_message, 
+                time=datetime.utcnow().time().strftime("%H:%M")
+            ),
+        )
+        return redirect("/")
+
+    return render_template("index.html", messages = messages)
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=False, threaded=True)
+    
